c7fb20eef2fc86c01e50d95556c2ad63f129ff43
SRT DEV <sahirruiz4@gmail.com>
2025-10-10 11:55:55 -0500

    Initial commit: Add Angular app and Terraform infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type        = string
+  default     = "my-angular-app"
+}
+
+variable "aws_access_key" {
+  type = string
+  sensitive = true
+}
+    
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_secret_key" {
+  type = string
+  sensitive = true
+}
