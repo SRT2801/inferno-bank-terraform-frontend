8ab7a3d8101c0655077835241259af0d2d30240a
SRT DEV <sahirruiz4@gmail.com>
2025-10-13 18:00:33 -0500

    feat: Mejorar la obtención del catálogo y mapear datos de la API

--- app/src/app/shared/services/catalog.service.ts
+++ app/src/app/shared/services/catalog.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Service } from '../models/service.interface';
+import { map } from 'rxjs/operators';
+import { Service, ApiResponse } from '../models/service.interface';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -11,6 +12,24 @@ export class CatalogService {
   constructor(private http: HttpClient) {}
 
   getCatalog(): Observable<Service[]> {
-    return this.http.get<Service[]>(environment.catalogApiUrl);
+    return this.http.get<any>(environment.catalogApiUrl).pipe(
+      map((response: any) => {
+        const items = response.response.data.items;
+        return items.map((item: any) => this.mapApiDataToService(item));
+      })
+    );
+  }
+
+  private mapApiDataToService(apiData: any): Service {
+    return {
+      id: apiData.ID,
+      categoria: apiData.Categoría,
+      proveedor: apiData.Proveedor,
+      servicio: apiData.Servicio,
+      plan: apiData.Plan,
+      precioMensual: apiData['Precio mensual'],
+      velocidadDetalles: apiData['Velocidad/Detalles'],
+      estado: apiData.Estado
+    };
   }
 }
