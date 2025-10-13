9166a81d42bb32ad1002eaa406c6700429ab1923
SRT DEV <sahirruiz4@gmail.com>
2025-10-13 17:07:14 -0500

    feat: Actualizar rutas para redirigir a home2 y agregar componente Home2

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { Home2 } from './home2/home2';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home2', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: '**', redirectTo: '/home' },
+  { path: 'home2', component: Home2 },
+  { path: '**', redirectTo: '/home2' },
 ];
