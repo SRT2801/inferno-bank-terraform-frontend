ca7757772c356dd43879d7f53cff4b65bd893b16
SRT DEV <sahirruiz4@gmail.com>
2025-10-13 18:00:11 -0500

    feat: Actualizar rutas para redirigir a Home en lugar de Home2

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
-import { Home2 } from './home2/home2';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home2', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'home2', component: Home2 },
-  { path: '**', redirectTo: '/home2' },
+  { path: '**', redirectTo: '/home' },
 ];
