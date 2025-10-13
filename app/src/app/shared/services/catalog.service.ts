02a40135dbe1cf54d5ad25b5d5122fdac95e90ba
SRT DEV <sahirruiz4@gmail.com>
2025-10-13 17:08:06 -0500

    feat: Corregir importaciones y actualizar la URL del API en CatalogService

--- app/src/app/shared/services/catalog.service.ts
+++ app/src/app/shared/services/catalog.service.ts
@@ -1,18 +1,16 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { Service } from "../models/service.interface";
-import { environment } from "../../../environments/environment.prod";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Service } from '../models/service.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class CatalogService {
-  private readonly apiUrl = `${environment.apiUrl}/catalog`;
-
   constructor(private http: HttpClient) {}
 
   getCatalog(): Observable<Service[]> {
-    return this.http.get<Service[]>(this.apiUrl);
+    return this.http.get<Service[]>(environment.catalogApiUrl);
   }
 }
