c5837009c7c5e39806e46bacf0808490e78bcb2e
SRT DEV <sahirruiz4@gmail.com>
2025-10-13 20:03:15 -0500

    feat: Agregar estrategia de detección de cambios OnPush al componente Header

--- app/src/app/shared/components/header/header.component.ts
+++ app/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,6 +7,7 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
   @Input() title: string = 'Inferno Bank Services';
