a990d2bf74acd1218e059b48cd8c44e05b426cd9
SRT DEV <sahirruiz4@gmail.com>
2025-10-15 09:23:31 -0500

    feat: Agregar ruta para el componente de registro en la configuración de rutas

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: 'home',
     component: HomeComponent,
