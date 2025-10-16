341a1433408abba529bcef1ff60946bfc070d1ff
SRT DEV <sahirruiz4@gmail.com>
2025-10-15 21:37:11 -0500

    feat: Añadir ruta para el componente de perfil con protección de autenticación

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
@@ -12,6 +13,11 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
     canActivate: [authGuard],
   },
   { path: '**', redirectTo: '/login' },
